b227d2706db3413a02f5cc30451aa305c10481ee
kelu <kelu124@gmail.com>
2024-04-22 12:51:45 +0200

    Tests of R2R

--- documentation/article/polyglot.sh
+++ documentation/article/polyglot.sh
@@ -1,1 +1,1 @@
-truepolyglot pdfzip --pdffile pic0rick.pdf --zipfile pic0doc.zip PDF_pic0rick.pdf
+truepolyglot szippdf --pdffile pic0rick.pdf --zipfile pic0doc.zip PDF_pic0rick.pdf
--- python/utils.py
+++
@@ -1,2 +0,0 @@
- #inspiration from https://github.com/kelu124/python-usb-un0rick
-
