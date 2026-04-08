90060e8cef11b75b16be6d6d102dfea8025bc762
Luc J <kelu124@gmail.com>
2026-04-08 19:11:29 +0200

    Updating script so to compile both for 2040 and rp2350

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,6 +1,12 @@
-       
-mkdir -p build
-cd build
-cmake ..
-make -j4
-cp adc-pulse.uf2 ../pigo_mux.uf2
+#!/bin/bash
+set -e
+
+# rp2040
+cmake -B build2040 -DPICO_BOARD=pico
+cmake --build build2040 -j4
+cp build2040/adc-pulse.uf2 rp2040.uf2
+
+# rp2350
+cmake -B build2350 -DPICO_BOARD=pico2
+cmake --build build2350 -j4
+cp build2350/adc-pulse.uf2 rp2350.uf2
